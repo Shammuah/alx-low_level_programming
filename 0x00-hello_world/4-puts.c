a3be3a968d1a05fedea7f965a53893b948b3a2d6
Shammuah <odumijeokonye@gmail.com>
2022-09-08 16:10:11 -0700

    Betty file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,11 @@
 /**
  * main - entry point
  *
- * Return zero on success
+ * Return: zero on success
+ *
  */
 int main(void)
 {
-	puts("\nProgramming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
