46cb59a9eba9f70184f2d4b420fc6da15f1b7e57
Shammuah <odumijeokonye@gmail.com>
2022-09-10 16:35:55 -0700

    Alphabets in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - alphabets in lowercase
+ *
+ * Return - 0 if successful
+ *
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar("\n");
+	return (0);
+}
