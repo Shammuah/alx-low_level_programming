db5d8458914e9a1e32e539011e0aa8bbcc422b24
Shammuah <odumijeokonye@gmail.com>
2022-09-10 16:45:00 -0700

    Alphabets in lowercase then in UPPERCASE

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - alphabets in lowercase then in uppercase
+ *
+ * Return: 0 if successful
+ *
+ */
+
+int main(void)
+{
+	char a = 'a';
+	while (a <= 'z')
+	{
+		putchar(a);
+		a++;
+	}
+	a = 'A';
+	while (a <= 'Z')
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
