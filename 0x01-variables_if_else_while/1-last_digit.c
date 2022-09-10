0947627e47b3964b2eaaa11143520c4798c2e04d
Shammuah <odumijeokonye@gmail.com>
2022-09-10 16:08:51 -0700

    Last digit less than or greter than 5

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ int main(void)
 	}
 	else if (last_digit == 0)
 	{
-		printf("Last digit of %d id %d and is 0\n", n, last_digit);
+		printf("Last digit of %d is %d and is 0\n", n, last_digit);
 	}
 	else
 	{
