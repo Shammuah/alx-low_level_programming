b365c76a717b179bfce3eebbaaee0e6bc32219e7
Shammuah <odumijeokonye@gmail.com>
2022-09-10 17:40:24 -0700

    Numbers in base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Numbers of base 16
+ *
+ * Return: 0 if successful
+ *
+ */
+
+int main(void)
+{
+	char a = 0;
+	
+	while (a < 10)
+	{
+		putchar(a);
+		a++;
+	}
+	
+	a = 'a';
+
+	while (a < g)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
