a3f90815dba052a7617987f94979db2176c85df4
Shammuah <odumijeokonye@gmail.com>
2022-09-15 16:45:04 -0700

    Absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _abs - Absolute value of an integer
+ *
+ * @n: Integer parameter
+ *
+ * Return: 0 if successful
+ *
+ */
+
+int _abs(int)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * -1);
+	}
+}
