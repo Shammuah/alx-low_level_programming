93d560d62a3d03401ab5050535cf8be8d4e4f299
Shammuah <odumijeokonye@gmail.com>
2022-09-15 18:38:49 -0700

    Checks for uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,13 @@
 
 /**
  * _isupper - Checks for uppercase character.
- *
+ * @c: Parameter
  * Return: Always 0.
  */
 
 int _isupper(int c)
 {
-	if (c >= 'A' || c <= 'Z')
+	if (c >= 'A' && c <= 'Z')
 	{
 		return (1);
 	}
