7e0c35764d62319c9bb3bca1291fbb4e6f7dc102
Shammuah <odumijeokonye@gmail.com>
2022-09-27 19:13:45 -0700

    Number of bytes in s from accept

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strspn - Returns number of bytes in segment s
+ * @s: String
+ * @accept: Bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, b = 0;
+
+	for (i = 0; s[i] >= '\0'; i++)
+	{
+		for (j = 0; accept[j] >= '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				b++;
+				break;
+			}
+		}
+		if (accept[j] = '\0')
+			break;
+	}
+	return (b);
+}
