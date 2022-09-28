047448acde0f9de2733fd287896375b425495545
Shammuah <odumijeokonye@gmail.com>
2022-09-27 18:15:19 -0700

    Pointer to memory area s

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memset - Returns pointer to memory area s
+ * @s: Memory area
+ * @b: Byte
+ * @n: Integer
+ * Return: Nothing
+ */
+
+char *_memset(char *s, char b, unsigner int n)
+{
+	unsigned int index;
+
+	for (index = 0; index < n; index+)
+	{
+		s[index] = b;
+	}
+	return (s);
+}
