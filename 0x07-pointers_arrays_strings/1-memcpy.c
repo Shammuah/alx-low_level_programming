373f069a77ad33936b6cc29854a921e96702dfed
Shammuah <odumijeokonye@gmail.com>
2022-09-27 18:31:43 -0700

    From mem area src to mem area dest

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _memcpy - Returns a pointer to dest
+ * @dest: Memory area to copy to
+ * @src: Memory area to copy from
+ * @n: Number of bytes
+ * Return: Nothing
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n);
+{
+	unsigned int index;
+
+	for (index = 0; index < n; index++)
+	{
+		dest[index] = src[index];
+	}
+	return(dest);
+}
