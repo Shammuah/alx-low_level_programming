ef329f65cbe188a65167ab4d39b38dd9e313555b
Shammuah <odumijeokonye@gmail.com>
2022-10-05 17:59:46 -0700

    Name of program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,7 +5,7 @@
  * main - Prints the name of the program
  * @argc: Number of command line arguments
  * @argv: Array name
- * Return: Always 0;
+ * Return: Always 0
  */
 
 int main(int argc, char* argv[])
