062ae543fd8706724cfd54a2619ba8003978ed98
Shammuah <odumijeokonye@gmail.com>
2022-10-05 18:37:44 -0700

    multiply first two integers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints product of first two integers
+ * @argc: Number of command line arguments
+ * @argv: Array of arguments
+ * Return: 0 if successful
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int j = 1;
+
+	if (argc == 1 || argc == 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; 1 < 3; i++)
+			j *= atoi(argv[i]);
+
+		printf("%d\n", j);
+	}
+
+	return (0);
+}
