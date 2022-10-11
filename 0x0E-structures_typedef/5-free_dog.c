375403181cd7bf65807986749e195dabc40993c4
Shammuah <odumijeokonye@gmail.com>
2022-10-10 19:10:56 -0700

    Free dog

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog - function to free dog
+ * @d: dog object
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->owner);
+		free(d->name);
+		free(d);
+	
+	}
+}
