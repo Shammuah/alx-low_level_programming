932e431b6b0341de9c80bc33f9ba3263a214395e
Shammuah <odumijeokonye@gmail.com>
2022-10-10 18:16:30 -0700

    Struct variable

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>>
+
+/**
+ * init_dog - function dog
+ * @d: dog object
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ * Return: dog if successful
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner){
+	if (d != NULL){
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}
