6f2669f43512a4a34097a21113f2fb5b0e5d2190
Shammuah <odumijeokonye@gmail.com>
2022-10-10 18:21:11 -0700

    Struct variable

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <stdio.h>
 #include <stdlib.h>
 
 /**
@@ -8,11 +7,12 @@
  * @name: Dog name
  * @age: Dog age
  * @owner: Dog owner
- * Return: dog if successful
  */
 
-void init_dog(struct dog *d, char *name, float age, char *owner){
-	if (d != NULL){
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
 		d->name = name;
 		d->age = age;
 		d->owner = owner;
